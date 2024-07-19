620e45e9fe02e0fa4f4da37c2c97a74a5124189e
Alejandro Hurtado Namimatsu <alejandrohurtado888@gmail.com>
2024-07-19 14:03:26 +0100

    Finalised JS script

--- Bin2Dec.js
+++ Bin2Dec.js
@@ -8,14 +8,14 @@ var bin_input = 10010101
 function binary_check(binary_input, arr){
   let character_count = binary_input.toString().length;
   if (character_count > 8) {
-    console.log("Please input a binary input shorter than 8 characters.")
+  //   console.log("Please input a binary input shorter than 8 characters.")
   };
 
   for (let i = 2; i < character_count; i++) {
     let iterator = 0 + i;
     let added_val = Math.pow(2, iterator);
     arr.push(added_val);
-    console.log(arr);
+//     console.log(arr);
   };
   return arr
 
@@ -36,21 +36,26 @@ function output_generator(unreversed){
 
 let bin_array = [1, 2];
 let final_bin = output_generator(bin_input);
+// console.log("Final bin = ")
+// console.log(final_bin)
 let checked_seq = binary_check(bin_input, bin_array);
 
 function Bin2Dec(bin_input, bin_array) {
   var answer = 0;
-  for (let bin in bin_input) {
-    for (let multiplier in bin_array) {
-      let dec = bin * multiplier;
-      console.log(dec);
-      var answer = answer + dec;
-    }
+  var bin_string = bin_input.toString()
+
+  for (let i = 0; i < bin_string.length; i++) {
+    let bin_int = Number(bin_string[i]);
+    let multiplier = bin_array[i];
+    let dec = bin_int * multiplier
+    var answer = dec + answer;
+
   }
+
   return answer;
 };
 
-console.log("Before the last seq");
-console.log(checked_seq);
+// console.log("Before the last seq");
+// console.log(checked_seq);
 let test_check = Bin2Dec(final_bin, checked_seq);
-console.log(test_check);
+return test_check;
