db48f7d011e371febc98a82dfeadf5c7fcab04d8
Alejandro Hurtado Namimatsu <alejandrohurtado888@gmail.com>
2024-07-19 17:34:50 +0100

    Finished Bin2Dec base challenge

--- Bin2Dec.js
+++ Bin2Dec.js
@@ -3,22 +3,29 @@
 // 3. Reverse input and separate them into individual items in a matrix/dictionary type structure.
 // 4. 2**i * items and then add them together
 
-var bin_input = 10010101
 
-function binary_check(binary_input, arr){
+function array_release(binary_input, arr){
+  let character_count = binary_input.toString().length;
+  for (let i = 2; i < character_count; i++) {
+      let iterator = 0 + i;
+      let added_val = Math.pow(2, iterator);
+      arr.push(added_val);
+  //     console.log(arr);
+    };
+    return arr
+
+}
+
+
+
+function binary_check(binary_input){
   let character_count = binary_input.toString().length;
   if (character_count > 8) {
-  //   console.log("Please input a binary input shorter than 8 characters.")
-  };
-
-  for (let i = 2; i < character_count; i++) {
-    let iterator = 0 + i;
-    let added_val = Math.pow(2, iterator);
-    arr.push(added_val);
-//     console.log(arr);
-  };
-  return arr
-
+    return false;
+  } else if (character_count <= 8){
+    return true;
+  } 
+  
 };
 
 function output_generator(unreversed){
@@ -34,11 +41,7 @@ function output_generator(unreversed){
   
 };
 
-let bin_array = [1, 2];
-let final_bin = output_generator(bin_input);
-// console.log("Final bin = ")
-// console.log(final_bin)
-let checked_seq = binary_check(bin_input, bin_array);
+
 
 function Bin2Dec(bin_input, bin_array) {
   var answer = 0;
@@ -57,5 +60,28 @@ function Bin2Dec(bin_input, bin_array) {
 
 // console.log("Before the last seq");
 // console.log(checked_seq);
-let test_check = Bin2Dec(final_bin, checked_seq);
-return test_check;
+let form = document.getElementById('bin-form')
+
+let bin_array = [1, 2];
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let bin_input = document.getElementById('bin').value;
+  let resultDiv = document.getElementById('result');
+
+  let final_bin = output_generator(bin_input);
+  let checked_seq = array_release(bin_input, bin_array);
+
+  let test_check = Bin2Dec(final_bin, checked_seq);
+
+  let allowed = binary_check(bin_input)
+
+  if (allowed === true) {
+    resultDiv.textContent = 'Binary: ' + test_check;
+  } else if (allowed === false){
+    resultDiv.textContent = 'Please input a binary thats shorter than 8 chars'
+
+  }
+});
